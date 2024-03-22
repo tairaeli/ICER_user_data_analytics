fc55001407aa692a3bcafdbc07ae7359e2ac5ef8
Elias Taira <tairaeli@msu.edu>
2024-03-22 16:13:40 -0400

    got a working model

--- ICER_User_Dat/data_exploration/clean_dat.sh
+++ ICER_User_Dat/data_exploration/clean_dat.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=5G
+#SBATCH --mem-per-cpu=50G
 #SBATCH --job-name CleanData
 
 conda activate data_sc
@@ -13,4 +13,4 @@ cd /mnt/home/tairaeli/ICER_project/ICER_user_data_analytics/ICER_User_Dat/data_e
 
 echo "running cleaning script"
 
-python gen_clean_slurm_dat.py
+python ./gen_clean_slurm_dat.py
