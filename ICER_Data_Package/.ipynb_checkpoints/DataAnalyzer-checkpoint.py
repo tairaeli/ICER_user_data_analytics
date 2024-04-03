3c2901629cfb0adc888ff15fc8a7084258996b8f
Shams <alkhali7@msu.edu>
2024-04-03 15:26:41 -0400

    commitin

--- ICER_Data_Package/.ipynb_checkpoints/DataAnalyzer-checkpoint.py
+++ ICER_Data_Package/.ipynb_checkpoints/DataAnalyzer-checkpoint.py
@@ -12,7 +12,7 @@ from sklearn.decomposition import PCA
 class DataAnalyzer:
 
     # add preprocessing=false parameter to specify if user want preprocessing then call preprocessing method
-    def __init__(self, gpfspath='', slurmpath=''):
+    def __init__(self, gpfspath='', slurmpath='', full_dataset=False):
         # initialize data to None
         self.gpfs_data = None
         self.slurm_data = None
@@ -24,12 +24,14 @@ class DataAnalyzer:
         
         # direct each data path to read_gpfs or read_slurm depending on path
         if gpfspath != '':
-            self.read_gpfs(gpfspath)
+            # either read in portion of dataset using read_gpfs or whole dataset through lazy_loading_gpfs
+            if not full_dataset:
+                self.read_gpfs(gpfspath)
+            else:
+                self.lazy_loading_gpfs(gpfspath)
             
         if slurmpath != '':
             self.read_slurm(slurmpath)
-        
-            
         
         
     def read_gpfs(self, gpfspath):
@@ -46,6 +48,54 @@ class DataAnalyzer:
         self.gpfs_data = pd.read_csv(gpfspath, header=None, names=column_names, delimiter=' ', nrows=1e5)
         return self.gpfs_data
 
+    def lazy_loading_gpfs(self, gpfspath):
+        self.gpfs_data = []  # List to store each row's data
+        max_rows = 1000  # or set to None for full data
+        row_counter = 0
+        
+        column_names = [
+            "Inode (file unique ID)", "KB Allocated", "File Size", 
+            "Creation Time in days from today", "Change Time in days from today", 
+            "Modification time in days from today", "Access time in days from today", 
+            "GID numeric ID for the group owner of the file", "UID numeric ID for the owner of the file"
+        ]
+        
+        with open(gpfspath, 'r') as file:
+            for line in file:
+                if max_rows and row_counter >= max_rows:
+                    break
+                values = line.strip().split(' ')
+                self.gpfs_data.append(dict(zip(column_names, values)))
+                row_counter += 1
+
+    def files_per_user_gpfs(self):
+        # THIS ONLY STREAMS 1,000 rows!!! change later!
+        # Ensure self.gpfs_data is populated
+        if not self.gpfs_data:
+            print("No GPFS data to aggregate.")
+            return
+        
+        # Aggregate file counts per user
+        file_counts_per_user = {}
+        for row in self.gpfs_data:
+            uid = row.get("UID numeric ID for the owner of the file")
+            if uid:
+                file_counts_per_user[uid] = file_counts_per_user.get(uid, 0) + 1
+                
+        # Sort and prepare for CDF plot
+        file_counts = list(file_counts_per_user.values())
+        file_counts.sort()
+        cumulative_counts = np.cumsum(file_counts)
+        cumulative_distribution = cumulative_counts / cumulative_counts[-1]
+        
+        # Plot
+        plt.figure(figsize=(10, 6))
+        plt.plot(file_counts, cumulative_distribution, drawstyle='steps-post')
+        plt.xlabel('Number of Files Owned by a User')
+        plt.ylabel('CDF')
+        plt.title('Cumulative Distribution of Files per User')
+        plt.grid(True)
+        plt.show()
 
     def read_slurm(self, slurmpath):
         # set slurm_data to outputed pandas dataset
@@ -125,25 +175,58 @@ class DataAnalyzer:
             a dataframe of users that are have a greater number of total files than the 
             file limit.
         '''
+        users_with_many_files_df = pd.DataFrame()  # Ensure the DataFrame is always initialized
+
+        # If gpfs_data is a DataFrame, use pandas functionality to aggregate
         if isinstance(self.gpfs_data, pd.DataFrame):
-             
-            # Group by the user ID and count the number of files for each user
             file_count_per_user = self.gpfs_data.groupby("UID numeric ID for the owner of the file").size()
-            
-            # Filter users who have more files than the file_limit
             users_with_many_files = file_count_per_user[file_count_per_user > file_limit]
-            
-            # Convert the filtered Series object to a DataFrame
-            users_with_many_files_df = users_with_many_files.reset_index()
-            
+            users_with_many_files_df = users_with_many_files.reset_index(name='Number of files')
             users_with_many_files_df.columns = ['UID numeric ID for the owner of the file', 'Number of files']
-            
-            # Sort by 'Number of files' in descending order
+    
+        # If gpfs_data is a list from streamed data, aggregate manually
+        elif isinstance(self.gpfs_data, list):
+            file_count_per_user = {}
+            for row in self.gpfs_data:
+                uid = row["UID numeric ID for the owner of the file"]
+                file_count_per_user[uid] = file_count_per_user.get(uid, 0) + 1
+    
+            # Filter and create DataFrame for users exceeding file_limit
+            users_with_many_files = {uid: count for uid, count in file_count_per_user.items() if count > file_limit}
+            users_with_many_files_df = pd.DataFrame(list(users_with_many_files.items()), columns=['UID numeric ID for the owner of the file', 'Number of files'])
+    
+        else:
+            print("ERROR: gpfs data was not defined or in an unrecognized format")
+            return None
+    
+        # Sort by 'Number of files' in descending order if DataFrame is not empty
+        if not users_with_many_files_df.empty:
             users_with_many_files_df.sort_values('Number of files', ascending=False, inplace=True)
-            return users_with_many_files_df
+    
+        return users_with_many_files_df
+    
+        # # Sort by 'Number of files' in descending order
+        # users_with_many_files_df.sort_values('Number of files', ascending=False, inplace=True)
+        # return users_with_many_files_df
+        
+        # if isinstance(self.gpfs_data, pd.DataFrame):
+             
+        #     # Group by the user ID and count the number of files for each user
+        #     file_count_per_user = self.gpfs_data.groupby("UID numeric ID for the owner of the file").size()
             
-        print("ERROR: gpfs data was not defined")
-        return None
+        #     # Filter users who have more files than the file_limit
+        #     users_with_many_files = file_count_per_user[file_count_per_user > file_limit]
+            
+        #     # Convert the filtered Series object to a DataFrame
+        #     users_with_many_files_df = users_with_many_files.reset_index()
+        #     users_with_many_files_df.columns = ['UID numeric ID for the owner of the file', 'Number of files']
+            
+        #     # Sort by 'Number of files' in descending order
+        #     users_with_many_files_df.sort_values('Number of files', ascending=False, inplace=True)
+        #     return users_with_many_files_df
+            
+        # print("ERROR: gpfs data was not defined")
+        # return None
         
     # slurm preprocessing method
 
